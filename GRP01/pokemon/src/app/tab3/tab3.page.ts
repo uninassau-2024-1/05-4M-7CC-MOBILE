f1a8904448284cc5b6c53d284466cd352e8fc2b1
Gabriel Santos <101734403+gabrielssantos1@users.noreply.github.com>
2024-05-24 13:39:30 -0300

    Add files via upload

---
+++ GRP01/pokemon/src/app/tab3/tab3.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { PokeAPIService } from '../services/poke-api.service';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+  pokemon: any ={
+    
+  }
+
+  constructor() {}
+
+}
